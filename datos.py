b8807677bc7f74112c3e4f91f1640b24c4b06628
hectorflores329 <hectorflores329@gmail.com>
2021-03-19 14:04:20 -0600

    Commit N

--- datos.py
+++ datos.py
@@ -1,16 +1,63 @@
+import urllib.request
+import git
 import pandas as pd
 import datetime
-import urllib.request
-import json
+import http.client, urllib.request, urllib.parse, urllib.error, base64
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
 import tweepy
+from azure.ai.textanalytics import TextAnalyticsClient
+from azure.core.credentials import AzureKeyCredential
+import requests
+import json  
+import codecs
+import numpy as np
+import wget
+import http.client, urllib.request, urllib.parse, urllib.error, base64
 
 def update():
-    descargarDatos()
-    generarArchivo()
+    print("Comenzó...")
+    try:
+        datasetFinalTweet()
+        print("Los tweet se cargaron correctamente...")
+    except:
+        print("Error al cargar los Tweet")
+    try:
+        bingNews()
+        print("Las noticias se cargaron correctamente...")
+    except:
+        print("Error al cargar los Noticias")
+    try:
+        guardarDataCovid()
+        print("Los datos de datacovid se cargaron correctamente...")
+    except:
+        print("Error al cargar los archivos del la organizacion")
+    guardarRepositorio()
 
     return
 
-def descargarDatos():
+def guardarRepositorio():
+    #Esta linea crea un objeto para manejar el repositorio alojado en la ruta
+    #Correspondinete al argumento entregado en String
+    repoLocal = git.Repo('C:\Users\hecto\Documents\GitHub\github-actions_HN')  
+    try:
+        #Agrego todos los archivos nuevos
+        repoLocal.git.add(".")
+        #Hace el commit con un comentario sobre el origen del mismo y la hora
+        repoLocal.git.commit(m='Update automatico via Actualizar ' + datetime.datetime.now().strftime("%m-%d-%Y %H-%M-%S"))
+        #Apuntamos al gitHub (online)
+        origin = repoLocal.remote(name='origin')
+        #Se hace el push (enviar los archivos al repositorio online)
+        origin.push()
+        #Mensaje simpatico que todo salio bien
+        print("Repositorio actualizado =)")
+    except:
+        #Da un mensaje de error al fallar
+        print("Error de GITHUB")    
+    return
+
+def guardarDataCovid():
     # Filename: Covid HN
     covid = "https://onedrive.live.com/download.aspx?cid=9f999e057ad8c646&page=view&resid=9F999E057AD8C646!92347&parId=9F999E057AD8C646!62371&authkey=!AkePW7UW1KXQkMM&app=Excel"
     
@@ -116,9 +163,9 @@ def get_tweetConFecha(user, api = APITWEET()):
     return list(api.user_timeline(screen_name = user, count= 10))
 
 
-def generarArchivo():
+def definirDatasetPorCuenta(cuenta):
 
-    lista = get_tweetConFecha("cristiano")
+    lista = get_tweetConFecha(cuenta)
     salida = []
     for i in lista:  
         jsonObject = i._json.copy()
@@ -138,6 +185,79 @@ def generarArchivo():
 
     data = pd.DataFrame(salida)
     data.to_excel("final.xlsx", index=False)
+
+def datasetFinalTweet():
+    cuentas = [
+                "saludhn",
+                "opsoms"
+                ]
+    salida = []
+    for i in cuentas:
+        salida.append(definirDatasetPorCuenta(i))
+    data = pd.concat(salida)
+    data = data.sort_values(by=['FechaAux'])
+    del data["FechaAux"]
+    # data.to_csv("../Datos_Honduras/tweeter/tweeter.csv", index=False)
+    data.to_csv("tweeter.csv", index=False)
+    return data
+
+def fechaCorrecta(i):
+    año = i[0:4]
+    mes = i[5:7]
+    dia = i[8:10]
+    hora = i[11:13]
+    minuto = i[14:16]
+    return dia + "-" + mes + "-" + año + " " + hora + ":" + minuto
+
+def reemplazarFinal(i):
+    return i.replace("&pid=News","")
+
+def bingNews(pais = "Honduras"):
+    #pais = "Chile"
+    headers = {
+        # Request headers
+        #'Ocp-Apim-Subscription-Key': 'b091fbaeb9f94255b542befc3ecff8b8',
+        'Ocp-Apim-Subscription-Key': 'a9b5b1527a7b43929d7e15a383b1583a',
+    }
+
+    params = urllib.parse.urlencode({
+        # Request parameters
+        'q':  'covid-19 coronavirus ' + pais + ' loc:hn FORM=HDRSC4',
+        'count': '40',
+        'offset': '0',
+        'mkt': 'es-US',
+        'safeSearch': 'Moderate',
+        "sortBy": "Date"
+    })
+
+    #conn = http.client.HTTPSConnection('api.cognitive.microsoft.com')
+    conn = http.client.HTTPSConnection('dataintelligence.cognitiveservices.azure.com')
+    conn.request("GET", "/bing/v7.0/news/search?%s" % params, "{body}", headers)
+    response = conn.getresponse()
+    #data = response.read()
+
+    decoded_data=codecs.decode(response.read(), 'utf-8-sig')
+    d = json.loads(decoded_data)
+    conn.close()
+    aux =  d['value']
+    salida = []
+    for i in aux:
+        try:
+            i["imagen"] = i["image"]["thumbnail"]["contentUrl"]
+            i["pais"] = "Chile"
+            try:
+                i["Fuente"] = i['provider'][0]["name"]
+            except:
+                pass
+            salida.append(i.copy())
+        except:
+            pass
+    data = pd.DataFrame(salida)[["name","url","description","datePublished","imagen","pais","Fuente"]]
+    data["datePublished"] = data["datePublished"].apply(fechaCorrecta)
+    data["imagen"] = data["imagen"].apply(reemplazarFinal)
+    data[::-1].to_csv("../Datos_Honduras/bing/News/Honduras.csv",index=False)
+    return
+
 
 if __name__ == '__main__':
     print('Empezando proceso de descarga.')
